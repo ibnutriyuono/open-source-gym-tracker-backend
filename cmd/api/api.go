df2257d80471ca2ab58f62eb02dc51cfa38b9785
ibnutriyuono <ibnutriyuono@gmail.com>
2025-05-27 12:06:02 +0700

    add: api driver

---
+++ cmd/api/api.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"caloria-backend/internal/controller/health"
+	"caloria-backend/internal/controller/user"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	// "gorm.io/gorm"
+)
+
+type application struct {
+	config config
+	// db     *gorm.DB
+	userController   *user.UserController
+	healthController *health.HealthController
+}
+
+type config struct {
+	address string
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	// r.Use(func(next http.Handler) http.Handler {
+	// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		defer func() {
+	// 			if err := recover(); err != nil {
+	// 				log.Printf("RECOVERED in custom middleware: %v", err)
+	// 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	// 			}
+	// 		}()
+	// 		next.ServeHTTP(w, r)
+	// 	})
+	// })
+
+	r.Use(middleware.Timeout(60 * time.Second))
+	// userController := &user.UserController{
+	// 	DB: app.db,
+	// }
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthController.HealthCheck)
+		r.Route("/users", func(r chi.Router) {
+			r.Get("/", app.userController.FindAll)
+			r.Post("/", app.userController.Create)
+			r.Put("/{id}", app.userController.Update)
+			r.Delete("/{id}", app.userController.Delete)
+			r.Get("/{id}", app.userController.FindById)
+		})
+	})
+
+	return r
+}
+
+func (app *application) run(mux http.Handler) error {
+	server := &http.Server{
+		Addr:         app.config.address,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("Server started at %s", app.config.address)
+
+	return server.ListenAndServe()
+}
---
+++ cmd/api/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"caloria-backend/internal/controller/user"
+	"caloria-backend/internal/env"
+	"expvar"
+	"fmt"
+	"log"
+	"runtime"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config := &config{
+		address: env.GetString("PORT", ":8080"),
+	}
+
+	dbHost := env.GetString("DB_HOST", "")
+	dbUser := env.GetString("DB_USER", "")
+	dbPassword := env.GetString("DB_PASSWORD", "")
+	dbName := env.GetString("DB_NAME", "")
+	dbPort := env.GetString("DB_PORT", "")
+	dbSslMode := env.GetString("DB_SSL_MODE", "")
+	dbTimeZone := env.GetString("DB_TIMEZONE", "")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", dbHost, dbUser, dbPassword, dbName, dbPort, dbSslMode, dbTimeZone)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database: " + err.Error())
+	}
+
+	app := &application{
+		config: *config,
+		// db:     db,
+		userController: &user.UserController{
+			DB: db,
+		},
+	}
+
+	mux := app.mount()
+	version := env.GetString("VERSION", "1.0.0")
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("failed to get sql.DB from gorm DB: %v", err)
+	}
+
+	expvar.NewString("version").Set(version)
+	expvar.Publish("database", expvar.Func(func() any {
+		stats := sqlDB.Stats()
+		return map[string]any{
+			"OpenConnections":   stats.OpenConnections,
+			"InUse":             stats.InUse,
+			"Idle":              stats.Idle,
+			"WaitCount":         stats.WaitCount,
+			"WaitDuration":      stats.WaitDuration.String(),
+			"MaxIdleClosed":     stats.MaxIdleClosed,
+			"MaxLifetimeClosed": stats.MaxLifetimeClosed,
+		}
+	}))
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+	expvar.Publish("mem_stats", expvar.Func(func() any {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	return map[string]any{
+		"Alloc":      m.Alloc,
+		"TotalAlloc": m.TotalAlloc,
+		"Sys":        m.Sys,
+		"NumGC":      m.NumGC,
+	}
+}))
+
+
+	log.Fatal(app.run(mux))
+}
