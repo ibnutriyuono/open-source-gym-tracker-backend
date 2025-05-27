e63f8d2cfac4d9290065320a0f1c5267adecc088
ibnutriyuono <ibnutriyuono@gmail.com>
2025-05-27 17:19:39 +0700

    refactor: make time as variable

--- internal/helper/token/jwt.go
+++ internal/helper/token/jwt.go
@@ -8,12 +8,15 @@ import (
 )
 
 func GenerateJWT(userID string, duration time.Duration) (string, error) {
+	now := time.Now()
+	
 	claims := jwt.MapClaims{
-		"sub":  userID,
-		"exp": time.Now().Add(duration).Unix(),
-		"iat": time.Now().Unix(),
+		"sub": userID,
+		"iat": now.Unix(),
+		"exp": now.Unix() + int64(duration.Seconds()), 
 	}
-	jwtSecret :=  env.GetString("JWT_SECRET", "")
+	
+	jwtSecret := env.GetString("JWT_SECRET", "")
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(jwtSecret))
 }
