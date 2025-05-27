b1a4c8eb7973e9edebe23017a4297867018c5693
ibnutriyuono <ibnutriyuono@gmail.com>
2025-05-27 12:06:11 +0700

    add: health controller

---
+++ internal/controller/health/health.controller.go
@@ -0,0 +1,31 @@
+package health
+
+import (
+	"caloria-backend/internal/helper/response"
+	"encoding/json"
+	"expvar"
+	"net/http"
+)
+
+type HealthController struct{}
+
+func (uc *HealthController) HealthCheck(w http.ResponseWriter, r *http.Request) {
+
+	selectedKeys := map[string]bool{
+		"version":         true,
+		"database":        true,
+		"goroutines":      true,
+		"mem_stats": true,
+	}
+
+	expvarMap := make(map[string]interface{})
+	expvar.Do(func(kv expvar.KeyValue) {
+		if selectedKeys[kv.Key] {
+			var value interface{}
+			_ = json.Unmarshal([]byte(kv.Value.String()), &value)
+			expvarMap[kv.Key] = value
+		}
+	})
+
+	response.SendJSON(w, http.StatusOK, expvarMap, "OK")
+}
