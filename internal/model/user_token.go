96d3977bb3ed00ca3ca12dc50b8472ab2cfec18c
ibnutriyuono <ibnutriyuono@gmail.com>
2025-05-27 16:05:56 +0700

    add: user token model

---
+++ internal/model/user_token.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"time"
+)
+
+type UserToken struct {
+	ID           string     `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	UserID       string     `gorm:"type:uuid;not null;index" json:"user_id"`
+	AccessToken  string     `gorm:"type:text;not null" json:"access_token"`
+	RefreshToken string     `gorm:"type:text;not null" json:"refresh_token"`
+	UserAgent    string     `gorm:"type:text" json:"user_agent,omitempty"`
+	IPAddress    string     `gorm:"type:text" json:"ip_address,omitempty"`
+	ExpiresAt    *time.Time `gorm:"type:timestamptz" json:"expires_at,omitempty"`
+	IsRevoked    bool       `gorm:"default:false" json:"is_revoked"`
+	CreatedAt    time.Time  `gorm:"autoCreateTime" json:"created_at"`
+
+	User User `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;" json:"-"`
+}
